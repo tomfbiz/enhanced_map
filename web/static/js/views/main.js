ec62df66dd16aa91a62a1479c6aa34340e9b09bb
Tom Freudenheim <tomfreudenheim@me.com>
2017-05-24 15:37:14 +0200

    Fix JS errors on pages without logout link also redirect after logging out

--- web/static/js/views/main.js
+++ web/static/js/views/main.js
@@ -1,10 +1,14 @@
 export default class MainView {
   mount() {
     //  This will be executed when the document loads...
-    document.getElementsByClassName("logout-link")[0].onclick = () => {
-      let xhttp = new XMLHttpRequest();
-      xhttp.open("DELETE","/logout",true);
-      xhttp.send();
+    let logout_link = document.getElementsByClassName("logout-link")
+    if (logout_link.length > 0) {
+      logout_link[0].onclick = () => {
+        let xhttp = new XMLHttpRequest();
+        xhttp.open("DELETE","/logout",false);
+        xhttp.send();
+        window.location.href = '/'
+      };
     };
   }
 }
