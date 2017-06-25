12c719f14e37705b987669dc011ae4d0fd29e2b0
Tom Freudenheim <tomfreudenheim@me.com>
2017-06-25 12:19:01 +0200

    save handle data back to db note -- had to change from using assigned to using session when checking user this is because not using pipe for API (since don't want flash) this change causes test failures. Should be able to fix using test dep

--- web/controllers/APImap_controller.ex
+++ web/controllers/APImap_controller.ex
@@ -8,7 +8,8 @@ defmodule EnhancedMap.APIMapController do
 
     map = Repo.get!(Map, id)
     changeset = Map.changeset(map, map_params)
-
+require IEx
+IEx.pry
     case Repo.update(changeset) do
       {:ok, _map} ->
         json(conn, %{status: :ok})
@@ -22,7 +23,9 @@ defmodule EnhancedMap.APIMapController do
   def verify_map_for_current_user(conn, _opts) do
     map_id = conn.params["id"]
     map = Repo.get(Map, map_id)
-    if conn.assigns[:current_user] && map.user_id == conn.assigns[:current_user].id do
+    session_current_user = get_session(conn, :current_user)
+
+    if session_current_user && map.user_id == session_current_user.id do
       conn
     else
       conn
